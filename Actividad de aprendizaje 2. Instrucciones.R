bd2b16a9302cc15d848e7a8703cdd3fa0b6cee87
EmilBalju <69735048+EmilBalju@users.noreply.github.com>
2022-02-02 20:48:06 -0600

    Add files via upload

---
+++ Actividad de aprendizaje 2. Instrucciones.R
@@ -0,0 +1,114 @@
+#Califcacion 3 pts.
+#Para leer la base de datos 
+library("dplyr")
+library("ggplot2movies")
+
+#########################
+##Ejercicio 1 (3 pts.)###
+#########################
+
+#La siguiente base de datos contiene información diversa sobre peliculas
+head(movies)
+#La funcion str les permite obtener un panorama de la base e datos en cuanto al tipo de variables
+str(movies)
+#La función da la descripción de cada una de las columnas de la base de datos
+?movies
+
+#a) Para las peliculas de acción que tengan un presupuesto disponible,
+#calcula la media de su duración y la media de su presupuesto utilizando
+#la funcion pipe 0.5 pts
+accion <- movies %>%
+  filter(Action==1) %>%
+  filter(!is.na(budget)) %>%
+  summarize(mean(length),mean(budget))
+  
+#b) Calcula el presupuesto promedio por cada año para todas las 
+#peliculas que tengan un presupuesto, utiliza la funcion pipe 0.5 pts
+presupuestopromedio <- movies %>%
+  filter(!is.na(budget)) %>%
+  group_by(year) %>%
+  summarize(mean(budget))
+
+#c) Crea una nueva columna que calcule el presupuesto por minuto 
+#de cada pelicula, luego calcula el precio promedio y la desviacion
+#estandar de la duracion por minuto para las peliculas que son 
+#de comedia y las que no lo son, usa la funcion pipe 0.5 pts. 
+movies2c <- movies %>%
+  mutate(presupuestoxmin=budget/length) %>%
+  group_by(!is.na(Comedy)) %>%
+  summarize(mean(budget),sd(length))
+  
+  
+
+#d) Calcula el rating promedio para las peliculas 
+#que  salieron a partir del año 2000 y ordenalos de menor  
+#a mayor, utiliza la función pipe  0.5 pts. 
+movies2d<- movies %>%
+  filter(year >=2000) %>%
+  arrange(year) %>%
+  summarize(mean(rating))
+
+#e) Crea un nuevo data frame con las columnas year, length, budget (simepre que hayan datos)
+#y rating, cambiando los nombres a ano, duracion, presupuesto, rating
+#ordena los anos del mas reciente al más antiguo, segudo de la duracióm
+#de la mas larga a la más corta, utiliza la función pipe 0.4 pts
+
+movies2e<- movies %>%
+  filter(!is.na(budget)) %>%
+  select(year,length,budget,rating) %>%
+  arrange(desc(year),desc(length)) %>%
+  rename(ano = year, duracion = length, presupuesto = budget, rating = rating)
+
+
+#f) Del ejercicio anterior, obtén el maximo de la 
+#duracion, presupuesto y rating para cada uno de los años para
+#los últimos 10 años, utilizando los nombres d.max, p.max
+#y r.max (Nota: no se puede usar la funcion rename), utiliza
+#la funcion pipe 0.1 pts
+
+movies2f <- movies2e %>%
+  filter(ano>=1996 & ano<=2005) %>%
+  summarize(d.max=max(duracion),p.max=max(presupuesto),r.max=max(rating))
+
+#g) Con los filtros del ejercicio 5 calcula el presupuesto por 
+#minuto para las peliculas que tienen rating mayor o igual a 8, 
+#posteriormente, calcula el presupuesto por minuto para cada año
+#utiliza el nombre p.min.p para esa columna, utiliza la función
+#pipe 0.5 pts. 
+
+movies2g <- movies2e %>%
+  filter(rating>=8) %>%
+  group_by(ano) %>%
+  summarize(p.min.p = (presupuesto/duracion))
+#########################
+##Ejercicio 2 (2 pts.)###
+#########################
+
+#Consideremos la base de datos con la que trabajamos en actividades anteriores: 
+bdtaxi<-read.csv("https://www.dropbox.com/s/cqvuu3byzxfk5hf/programacion.csv?dl=1", header=TRUE)
+str(bdtaxi)
+
+#a) Genera un resumen en donde la primer columna contenga a los niveles del factor "Tipo" y la segunda 
+#columna tenga la suma de la columna Total  (0.5 pts.)
+
+bdtaxi2a <- bdtaxi %>%
+  group_by(Tipo) %>%
+  summarize(sum(Total))
+
+#b) Crea un resumen en donde la primer columna contenga el mes correspondiente, la segunda la suma 
+#de los pasajeros transportados en dicho mes (con el nombre de columna Pasajeros.Ida) y la tercera
+#el la media de la columna total (con el nombre e columna Ingreso.Promedio) (0.5 pts.)
+
+bdtaxi2b <- bdtaxi %>%
+  group_by(Mes) %>%
+  summarize(Pasajeros.ida=sum(!is.na(Pasajeros)),Ingreso.promedio=mean(Total)) 
+
+#c) Se quiere conocer el monto total de cuotas pagadas por mes, general un tibble que de respuesta a esta 
+#interrongante (0.5 pts.)
+
+
+#d) Se desea conocer por mes, a cuanto ascienden los ingresos y gastos totales, excluyendo los conceptos de 
+#bono y cuotas de los gastos y manteniendo todo lo demas. Generar un tibble en donde la primer columna muestre
+#el mes en cuestion, la segunda columna debe de indicar si se trata del ingreso o del gasto y la última 
+#columna debe mostrar la cifra a la que asciende. #0.5 pts. 
+
